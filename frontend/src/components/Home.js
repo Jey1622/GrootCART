961bb7418665f78761c5359c70b2fe3bf4791a28
Jeyaraman S <jeyaramanapr22@gmail.com>
2025-05-01 22:54:52 +0530

    Loader Added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,8 @@
 import { Fragment, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../actions/productsAction";
+import Loader from "./layouts/Loader";
+import Product from "./product/Product";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -13,43 +15,22 @@ export default function Home() {
 
   return (
     <Fragment>
-      <h1 id="products_heading">Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <h1 id="products_heading">Latest Products</h1>
 
-      <section id="products" className="container mt-5">
-        <div className="row">
-          {products &&
-            products.map((product) => (
-              <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-                <div className="card p-3 rounded">
-                  <img
-                    className="card-img-top mx-auto"
-                    src={product.images[0].image}
-                  />
-                  <div className="card-body d-flex flex-column">
-                    <h5 className="card-title">
-                      <a href="">{product.name}</a>
-                    </h5>
-                    <div className="ratings mt-auto">
-                      <div className="rating-outer">
-                        <div
-                          className="rating-inner"
-                          style={{ width: `${(product.rating / 5) * 100}%` }}
-                        ></div>
-                      </div>
-                      <span id="no_of_reviews">
-                        ({product.numOfReviews} Reviews)
-                      </span>
-                    </div>
-                    <p className="card-text">&#8377;{product.price}</p>
-                    <a href="#" id="view_btn" className="btn btn-block">
-                      View Details
-                    </a>
-                  </div>
-                </div>
-              </div>
-            ))}
-        </div>
-      </section>
+          <section id="products" className="container mt-5">
+            <div className="row">
+              {products &&
+                products.map((product) => (
+                  <Product product={product} />
+                ))}
+            </div>
+          </section>
+        </Fragment>
+      )}
     </Fragment>
   );
 }
