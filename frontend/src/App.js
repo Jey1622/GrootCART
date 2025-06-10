63dde831a5396b5f62ed370a824a92f199d9b8ea
Jeyaraman S <jeyaramanapr22@gmail.com>
2025-06-10 07:28:53 +0530

    Added a shipping,confirm order and payment component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import ProductDetail from "./components/product/ProductDetail";
 import ProductSearch from "./components/product/ProductSearch";
 import Login from "./components/user/Login";
 import Register from "./components/user/Register";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import store from "./store";
 import { loadUser } from "./actions/userAction";
 import Profile from "./components/user/Profile";
@@ -17,12 +17,26 @@ import UpdateProfile from "./components/user/UpdateProfile";
 import UpdatePassword from "./components/user/UpdatePassword";
 import ForgotPassword from "./components/user/ForgotPassword";
 import ResetPassword from "./components/user/ResetPassword";
+import Cart from "./components/cart/Cart";
+import Shipping from "./components/cart/Shipping";
+import ConfirmOrder from "./components/cart/ConfirmOrder";
+import Payment from "./components/cart/Payment";
+import axios from "axios";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 function App() {
+  const [stripeApiKey, setStripeApiKey] = useState("");
 
-  useEffect(()=>{
-    store.dispatch(loadUser)
-  })
+  useEffect(() => {
+    store.dispatch(loadUser);
+
+    async function getStripeApiKey() {
+      const { data } = await axios.get("/api/v1/stripeapi");
+      setStripeApiKey(data.stripeApiKey);
+    }
+    getStripeApiKey();
+  }, []);
 
   return (
     <Router>
@@ -36,11 +50,61 @@ function App() {
             <Route path="/product/:id" element={<ProductDetail />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/myprofile" element={<ProtectedRoute><Profile/></ProtectedRoute> } />
-            <Route path="/myprofile/update" element={<ProtectedRoute><UpdateProfile/></ProtectedRoute> } />
-            <Route path="/myprofile/update/password" element={<ProtectedRoute><UpdatePassword/></ProtectedRoute> } />
-            <Route path="/password/forgot" element={<ForgotPassword/> } />
-            <Route path="/password/reset/:token" element={<ResetPassword/> } />
+            <Route
+              path="/myprofile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/myprofile/update"
+              element={
+                <ProtectedRoute>
+                  <UpdateProfile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/myprofile/update/password"
+              element={
+                <ProtectedRoute>
+                  <UpdatePassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/password/forgot" element={<ForgotPassword />} />
+            <Route path="/password/reset/:token" element={<ResetPassword />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route
+              path="/shipping"
+              element={
+                <ProtectedRoute>
+                  <Shipping />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/order/confirm"
+              element={
+                <ProtectedRoute>
+                  <ConfirmOrder />
+                </ProtectedRoute>
+              }
+            />
+            {stripeApiKey && (
+              <Route
+                path="/payment"
+                element={
+                  <ProtectedRoute>
+                    <Elements stripe={loadStripe(stripeApiKey)}>
+                      <Payment />
+                    </Elements>
+                  </ProtectedRoute>
+                }
+              />
+            )}
           </Routes>
         </div>
         <Footer />
